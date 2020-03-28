6fc10983e8c0d7ba4bb952f904671ff3e70cf042
miqilin21 <994718917@qq.com>
2020-03-28 15:08:41 +0800

    rafactor: recordListModel.ts

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,13 +1,17 @@
 const localStorageKeyName = "recordList";
 const recordListModel = {
-  clone(data: RecordItem[] | RecordItem){
+  data: [] as RecordItem[],
+  clone(data: RecordItem[] | RecordItem) {
     return JSON.parse(JSON.stringify(data));
   },
   fetch() {
-    return JSON.parse(window.localStorage.getItem(localStorageKeyName) || "[]") as RecordItem[];
+    this.data = JSON.parse(
+      window.localStorage.getItem(localStorageKeyName) || "[]"
+    ) as RecordItem[];
+    return this.data;
   },
-  save(data: RecordItem[]) {
-    window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
+  save() {
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
   }
 };
 
