5e590a4b87456daf75d7be1570f22ed28880a29c
miqilin21 <994718917@qq.com>
2020-03-30 13:59:54 +0800

    delete: Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -16,14 +16,13 @@
 <script lang="ts">
 import Vue from "vue";
 import NumberPad from "@/components/Money/NumberPad.vue";
-import Types from "@/components/Money/Types.vue";
 import FormItem from "@/components/Money/FormItem.vue";
 import Tags from "@/components/Money/Tags.vue";
 import { Component } from "vue-property-decorator";
 import Tabs from "@/components/Tabs.vue";
 import recordTypeList from "@/constants/recordTypeList";
 @Component({
-  components: { Tabs, Tags, FormItem, Types, NumberPad }
+  components: { Tabs, Tags, FormItem, NumberPad }
 })
 export default class Money extends Vue {
   get recordList() {
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -27,14 +27,13 @@
 </style>
 
 <script lang="ts">
-import Types from "@/components/Money/Types.vue";
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import Tabs from "@/components/Tabs.vue";
 import intervalList from '@/constants/intervalList';
 import recordTypeList from '@/constants/recordTypeList';
 @Component({
-  components: { Tabs, Types }
+  components: { Tabs }
 })
 export default class Statistics extends Vue {
   type = "-";
